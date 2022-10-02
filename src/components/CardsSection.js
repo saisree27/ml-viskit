180a47937697629a2af8dc677f949866ffd422ac
ericsheng495 <eric.sheng495@gmail.com>
2022-10-01 21:23:34 -0400

    adding modal

--- src/components/CardsSection.js
+++ src/components/CardsSection.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import { Link } from "react-router-dom";
+import Modal from "./Modal";
+import { useState } from "react";
 import "./CardsSection.css";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 const CardsSection = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
     <div className="cardsSection-container">
       <div className="card-container">
@@ -17,7 +32,7 @@ const CardsSection = () => {
         <Card name="name" description="description" />
       </div>
       <div className="submit-container">
-        <button className="btn btn-light">Submit</button>
+        <Modal />
       </div>
     </div>
   );
