b60d0502b40a764dd877a09528058e197077b19f
ShDhruvSh <67289288+ShDhruvSh@users.noreply.github.com>
2022-10-02 03:11:16 -0400

    Pushing attempt at React using Node API

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/login.css';
 import { useNavigate } from 'react-router-dom';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function Login() {
+
+    const [userName, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [body, setBody] = useState("")
+
+
+
+    
+
     const navigate = useNavigate();
+
+    const querySignUp = async e => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:8080/api/auth/signup', {
+            body: {username: userName,
+            email: email,
+            password: password},
+            body: JSON.stringify({ post: this.state.post })
+          });
+          const body = await response.text();
+          
+          this.setState({ responseToPost: body });
+          console.log(body);
+        };
+
     return (
         <div>
             <Navbar />
@@ -16,17 +41,18 @@ export default function Login() {
                     <div class="signup">
                         <form>
                             <label for="chk" aria-hidden="true">Sign Up</label>
-                            <input type="text" name="txt" placeholder="User name" required=""/>
-                            <input type="password" name="pswd" placeholder="Password" required=""/>
-                            <button onClick={() => navigate('/home')}>Sign Up</button>
+                            <input type="text" name="txt" placeholder="Email" required="" onChange={(e) => setEmail(e.target.value)}/>
+                            <input type="text" name="txt" placeholder="User name" required="" onChange={(e) => setUserName(e.target.value)}/>
+                            <input type="password" name="pswd" placeholder="Password" required="" onChange={(e) => setPassword(e.target.value)}/>
+                            <button onClick={() => querySignUp()}>Sign Up</button>
                         </form>
                     </div>
 
                     <div class="login">
                         <form>
                             <label for="chk" aria-hidden="true">Login</label>
-                            <input type="text" name="txt" placeholder="User name" required=""/>
-                            <input type="password" name="pswd" placeholder="Password" required=""/>
+                            <input type="text" name="txt" placeholder="User name" required=""  onChange={(e) => setUserName(e.target.value)}/>
+                            <input type="password" name="pswd" placeholder="Password" required=""  onChange={(e) => setPassword(e.target.value)}/>
                             <button onClick={() => navigate('/home')}>Login</button>
                         </form>
                     </div>
