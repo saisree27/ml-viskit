bd76b1e5f9781e5e8e20da93da8dc5cdbc3d55df
saisree27 <saigautambonam@gmail.com>
2022-10-02 08:17:56 -0400

    Update input css to avoid coupling

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import '../css/login.css';
-import { useNavigate } from 'react-router-dom';
+import "../css/login.css";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -16,22 +16,19 @@ import {
   logInWithEmailAndPassword,
 } from "../firebase";
 
-
 export default function Login() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [name, setName] = useState("");
-    const [user, loading, error] = useAuthState(auth);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [user, loading, error] = useAuthState(auth);
 
-    useEffect(() => {
-        if (user) navigate("/home");
-      }, [user]);
+  useEffect(() => {
+    if (user) navigate("/home");
+  }, [user]);
 
-    
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    /*const querySignUp = async e => {
+  /*const querySignUp = async e => {
         e.preventDefault();
         const response = await fetch('http://localhost:8080/api/auth/signup', {
             body: {username: userName,
@@ -45,36 +42,80 @@ export default function Login() {
           console.log(body);
         };*/
 
-    return (
-        <div>
-            <Navbar />
-            <body class="body">
-            <div class="main-login">  	
-                <input type="checkbox" id="chk" aria-hidden="true"/>
+  return (
+    <div>
+      <Navbar />
+      <body class="body">
+        <div class="main-login">
+          <input type="checkbox" id="chk" aria-hidden="true" />
 
-                    <div class="signup">
-                        <form>
-                            <label for="chk" aria-hidden="true">Sign Up</label>
-                            <input type="text" name="txt" placeholder="Name" required=""  onChange={(e) => setName(e.target.value)}/>
-                            <input type="text" name="txt" placeholder="Email" required="" onChange={(e) => setEmail(e.target.value)}/>
-                            <input type="password" name="pswd" placeholder="Password" required="" onChange={(e) => setPassword(e.target.value)}/>
-                            <button onClick={() => registerWithEmailAndPassword(name, email, password)}>Sign Up</button>
-                            <button onClick={signInWithGoogle}>Sign Up With Google</button>
-                        </form>
-                    </div>
+          <div class="signup">
+            <form>
+              <label for="chk" aria-hidden="true">
+                Sign Up
+              </label>
+              <input
+                type="text"
+                name="txt"
+                placeholder="Name"
+                required=""
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="text"
+                name="txt"
+                placeholder="Email"
+                required=""
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required=""
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                onClick={() =>
+                  registerWithEmailAndPassword(name, email, password)
+                }
+              >
+                Sign Up
+              </button>
+              <button onClick={signInWithGoogle}>Sign Up With Google</button>
+            </form>
+          </div>
 
-                    <div class="login">
-                        <form>
-                            <label for="chk" aria-hidden="true">Login</label>
-                            <input type="text" name="txt" placeholder="Email" required=""  onChange={(e) => setEmail(e.target.value)}/>
-                            <input type="password" name="pswd" placeholder="Password" required=""  onChange={(e) => setPassword(e.target.value)}/>
-                            <button onClick={() => logInWithEmailAndPassword(email, password)}>Log In</button>
-                            <button onClick={signInWithGoogle}>Log In With Google</button>
-                        </form>
-                    </div>
-            </div>
-            </body>
-            <Footer/>
+          <div class="login">
+            <form>
+              <label for="chk" aria-hidden="true">
+                Login
+              </label>
+              <input
+                type="text"
+                name="txt"
+                placeholder="Email"
+                required=""
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required=""
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                onClick={() => logInWithEmailAndPassword(email, password)}
+              >
+                Log In
+              </button>
+              <button onClick={signInWithGoogle}>Log In With Google</button>
+            </form>
+          </div>
         </div>
+      </body>
+      <Footer />
+    </div>
   );
 }
